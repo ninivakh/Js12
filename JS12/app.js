3f1d2947937abb02a5977494555e8c77a4d64b65
ninivakh <120251081+ninivakh@users.noreply.github.com>
2023-06-15 21:47:03 +0300

    Add files via upload

---
+++ JS12/app.js
@@ -0,0 +1,9 @@
+function helloworld(){
+    let x = document.getElementById("number").value;
+    let y = 0;
+
+    while(y < x) {
+        document.write("Hello World! <br>");
+        y++;
+    }
+}
